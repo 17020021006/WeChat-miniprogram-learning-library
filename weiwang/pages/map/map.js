84cc8bd71caff5f9d28ca2c41d41be1a37f9c008
轰雨焦衔冻 <32992484+17020021006@users.noreply.github.com>
2019-05-29 16:33:26 +0800

    Add files via upload

---
+++ weiwang/pages/map/map.js
@@ -0,0 +1,15 @@
+// pages/map/map.js
+Page({
+  data:{
+
+markers:[{
+      iconPath: "../images/mark.png",
+      id: 0,
+      latitude: 23.362490,
+      longitude: 116.715790,
+      title:'华乾大厦',    
+    }],
+
+  }
+ 
+})
