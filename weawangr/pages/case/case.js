fed1449b29884c2e67df96bf130747f24b4037b6
轰雨焦衔冻 <32992484+17020021006@users.noreply.github.com>
2019-05-30 20:30:44 +0800

    Add files via upload

---
+++ weawangr/pages/case/case.js
@@ -0,0 +1,24 @@
+Page({
+  data: {
+    imgUrls: [
+      '../images/banner1.jpg',
+      '../images/banner2.jpg',
+      '../images/banner3.jpg',
+            ],
+    danmuList: [
+      {
+        text: '第 1s 出现的弹幕',
+        color: '#ff0000',
+        time: 1
+      },
+      {
+        text: '第 3s 出现的弹幕',
+        color: '#ff00ff',
+        time: 3
+      }]
+  
+  }
+
+  
+ 
+})
---
+++ weawangr/pages/map/map.js
@@ -0,0 +1,15 @@
+// pages/map/map.js
+Page({
+  data:{
+
+markers:[{
+      iconPath: "../images/mark.png",
+      id: 0,
+  latitude: 36.16508553931523,
+  longitude: 120.49558997154236,
+      title:'中国海洋大学',    
+    }],
+
+  }
+ 
+})
